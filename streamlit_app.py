cacc7291cd442000aadeee48152fb914dc43b220
danielwoolf <daniel.woolf@gmail.com>
2023-08-17 12:22:09 +0100

    Update streamlit_app.py
    Move the Fruit Load List Query and Load into a Button Action

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,6 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -32,8 +31,7 @@ def get_fruityvice_data(this_fruit_choice):
    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
    # Display a dataframe as an interactive table.
    return fruityvice_normalized
-                        
-                        
+                                          
 #New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
@@ -46,17 +44,20 @@ try:
 except URLError as e:
   streamlit.error()
 
-#dont' run anything past here whilst we troubleshoot
-streamlit.stop()
-
-
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_row = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_row)
+#snowflake-realted functions
+def get_fruit_load_list():
+      with my_cnx.cursor() as my_cur:
+         my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+         return my_cur.fetchall()
+
+# Add a button to load the fruit
+if streamlit.button('Get Fruit Load List'):
+   my_cnx = snowflake.connector.connnect(**streamlit.secrets["snowflake"])
+   my_data_rows = get_fruit_load_list();
+   streamlit.dataframe(my_data_rows)
+
+streamlit.stop()
 
 # Allow user to add fruit
 add_my_fruit= streamlit.text_input('What fruit would you like to add?')
